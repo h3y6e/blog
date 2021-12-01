634425e4dcca7ae0e32511224edf0ea98fd13d1d
5ebec <hash.a5ebec@gmail.com>
2021-12-02 00:43:41 +0900

    add postcss-color-mod-function

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,9 @@
 module.exports = () => ({
   plugins: [
     require("postcss-import"),
+    require("postcss-color-mod-function"),
     require("postcss-preset-env")({
       stage: 0,
-      features: {
-        "color-mod-function": true,
-      },
     }),
   ],
 });
