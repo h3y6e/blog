9660c618fcab5b28fde3f91ef658de8086dac2a6
h3y6e <h3y6e@pm.me>
2022-12-13 11:08:55 +0900

    tweak

--- theme/scripts/themeswitcher.js
+++ theme/scripts/themeswitcher.js
@@ -1,15 +1,15 @@
+var storageKey = "theme";
+var rootElement = document.documentElement;
+var themeSwitcher = document.getElementById("theme-switcher");
+var storedTheme = localStorage.getItem(storageKey);
+
 function toggle(theme) {
   return theme === "dark" ? "light" : "dark";
 }
 
-var rootElement = document.documentElement;
-var themeSwitcher = document.getElementById("theme-switcher");
-var storedTheme = localStorage.getItem("theme") || "dark";
-
 themeSwitcher.innerText = toggle(storedTheme);
-rootElement.setAttribute("data-theme", storedTheme);
 
-const tweets = document.querySelectorAll(".twitter-tweet");
+var tweets = document.querySelectorAll(".twitter-tweet");
 tweets.forEach(function (tweet) {
   tweet.setAttribute("data-theme", storedTheme);
 });
@@ -22,7 +22,7 @@ themeSwitcher.onclick = function () {
     targetTheme === "dark" ? "#a5ebec" : "#2aa298";
   themeSwitcher.innerText = theme;
   rootElement.setAttribute("data-theme", targetTheme);
-  localStorage.setItem("theme", targetTheme);
+  localStorage.setItem(storageKey, targetTheme);
 
   const tweets = document.querySelectorAll("[data-tweet-id]");
   tweets.forEach(function (tweet) {
